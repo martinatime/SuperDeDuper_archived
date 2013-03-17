4a98e53622b58a569e1b0cd657a34fd816b79c57
Aaron M <registration@martinatime.com>
2013-03-17 13:39:02 -0400

    Changed to use Apache Commons CLI to do argument parsing Fixed the tests related to this

--- SuperDeDuper/src/org/superdeduper/SuperDeDuper.java
+++ SuperDeDuper/src/org/superdeduper/SuperDeDuper.java
@@ -9,12 +9,18 @@ package org.superdeduper;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.superdeduper.parameters.ExecutionArguments;
-import org.superdeduper.parameters.Occurrence;
-import org.superdeduper.parameters.Parameter;
-import org.superdeduper.parameters.ValueInstance;
-import org.superdeduper.parameters.ValueState;
-import org.superdeduper.parameters.ValueType;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.lang3.StringUtils;
+//import org.superdeduper.parameters.ExecutionArguments;
+//import org.superdeduper.parameters.Occurrence;
+//import org.superdeduper.parameters.Parameter;
+//import org.superdeduper.parameters.ValueInstance;
+//import org.superdeduper.parameters.ValueState;
+//import org.superdeduper.parameters.ValueType;
 import org.superdeduper.parameters.exception.ParameterException;
 
 /**
@@ -25,26 +31,41 @@ public class SuperDeDuper {
 	private final static String CLASS_NAME = SuperDeDuper.class.getName();
 	private final static Logger LOGGER = Logger.getLogger(CLASS_NAME);
 	
-	private ExecutionArguments executionArguments = null;
+	private CommandLine executionArguments = null;
+	private String[] rawCommandLineInput = null;
 
 	/**
 	 * 
 	 * @param args
 	 */
-	public SuperDeDuper(String[] args) throws ParameterException {
-		executionArguments = new ExecutionArguments();
-		executionArguments.addParameterDefinition(Parameter.SCAN_DIRECTORY, Occurrence.REQUIRED, ValueState.VALUE_PRESENT, ValueType.STRING, ValueInstance.MULTIPLE);
-		executionArguments.addParameterDefinition(Parameter.PROPERTIES_OVERRIDE, Occurrence.OPTIONAL, ValueState.VALUE_PRESENT, ValueType.STRING, ValueInstance.SINGLE);
-		executionArguments.parse(args, false);
+	public SuperDeDuper(String[] args) {
+		rawCommandLineInput = args;
+		
+		//executionArguments = new ExecutionArguments();
+		//executionArguments.addParameterDefinition(Parameter.SCAN_DIRECTORY, Occurrence.REQUIRED, ValueState.VALUE_PRESENT, ValueType.STRING, ValueInstance.MULTIPLE);
+		//executionArguments.addParameterDefinition(Parameter.PROPERTIES_OVERRIDE, Occurrence.OPTIONAL, ValueState.VALUE_PRESENT, ValueType.STRING, ValueInstance.SINGLE);
+		//executionArguments.parse(args, false);
 	}
 	
 	/**
-	 * 
-	 * @return
+	 * This is the main method's logic repository so that the main can be tested. There is an expectation
+	 * that the arguments (the main's String[]) will be set in the constructor so that this method can use
+	 * them. (Tentative)This method will return null if no arguments are passed in. The assumption is that
+	 * triggers the help output. Other output may be returned based on the input. This is TO BE DETERMINED.
+	 *  
+	 * @return null if the input arguments are null/empty
 	 */
 	public Object run() {
 		String methodName = "run";
 		LOGGER.entering(CLASS_NAME, methodName);
+		
+		try {
+			this.executionArguments = parseCommandLine();
+		} catch (ParseException pe) {
+			LOGGER.logp(Level.SEVERE, CLASS_NAME, methodName, "There was an exception while parsing the Command Line. Exception=" + pe);
+			// skipping the rest and returning null
+		}
+		/*
 		if (executionArguments != null) {
 			if (LOGGER.isLoggable(Level.FINEST)) {
 				LOGGER.logp(Level.FINEST, CLASS_NAME, methodName, "Execution arguments: {0}", executionArguments);
@@ -54,15 +75,24 @@ public class SuperDeDuper {
 				LOGGER.logp(Level.FINEST, CLASS_NAME, methodName,"no Execution Args were passed in");
 			}
 		}
+		*/
 		LOGGER.exiting(CLASS_NAME, methodName);
 		return null;
+	}
+	
+	private CommandLine parseCommandLine() throws ParseException {
+		Options commandLineOptions = new Options();
+		commandLineOptions.addOption("s", "directory", true, "Scan directory - Directory to scan for audio files to process");
+		commandLineOptions.addOption("p", "settings", true, "Settings override - Location and filename of the file containing setting overrides");
+		CommandLineParser parser = new BasicParser();
+		return parser.parse(commandLineOptions, rawCommandLineInput);
 	}
 
 	/**
 	 * 
 	 * @return
 	 */
-	public ExecutionArguments getExecutionArguments() {
+	public CommandLine getExecutionArguments() {
 		return executionArguments;
 	}
 
@@ -72,16 +102,14 @@ public class SuperDeDuper {
 	 */
 	public static void main(String[] args) {
 		String methodName = "main";
+		LOGGER.entering(CLASS_NAME, methodName);
 		// This creates an instance for easier testing and possible
 		// future embedding of this class in other projects
-		SuperDeDuper runner = null;
-		try {
-			runner = new SuperDeDuper(args);
-			runner.run();
-		} catch(ParameterException pe) {
-			if(LOGGER.isLoggable(Level.SEVERE)) {
-				LOGGER.logp(Level.SEVERE, CLASS_NAME, methodName, pe.getMessage(), pe);
-			}
+		SuperDeDuper runner = new SuperDeDuper(args);
+		if (LOGGER.isLoggable(Level.FINE)) {
+			LOGGER.logp(Level.FINE, CLASS_NAME, methodName, "About to call run with arguments=[" + StringUtils.join(args, ',') + "]");
 		}
+		runner.run();
+		LOGGER.exiting(CLASS_NAME, methodName);
 	}
 }
--- SuperDeDuper/test/test/org/superdeduper/execution/MainTest.java
+++ SuperDeDuper/test/test/org/superdeduper/execution/MainTest.java
@@ -1,10 +1,12 @@
 package test.org.superdeduper.execution;
 
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import junit.framework.TestCase;
 
+import org.apache.commons.cli.Option;
 import org.junit.Before;
 import org.junit.Test;
 import org.superdeduper.SuperDeDuper;
@@ -19,18 +21,55 @@ public class MainTest extends TestCase {
 	protected void setUp() throws Exception {
 		super.setUp();
 	}
-
+	
 	@Test
-	public void testSuperDeDuper() throws ParameterException {
-		SuperDeDuper testNullArgs = new SuperDeDuper(null);
-		assertEquals(0, testNullArgs.getExecutionArguments().getParsedParameters().size());
+	public void testScanDirectory() {
+		SuperDeDuper testScanDirShort = new SuperDeDuper(new String[] {"-s", "scan-directory"});
+		assertNull(testScanDirShort.run());
+		assertNotNull(testScanDirShort.getExecutionArguments());
+		Option testOpt = new Option("s", "directory", true, "Scan directory - Directory to scan for audio files to process");
+		Option[] comparison = new Option[] {testOpt};
+		assertTrue(Arrays.equals(comparison, testScanDirShort.getExecutionArguments().getOptions()));
+		Option[] actuals = testScanDirShort.getExecutionArguments().getOptions();
+		assertEquals("scan-directory", actuals[0].getValue());
+		assertTrue(Arrays.equals(new String[0], testScanDirShort.getExecutionArguments().getArgs()));
+		// cleanup
+		testScanDirShort = null;
 		
-		SuperDeDuper testNonNullArgs = new SuperDeDuper(new String[] {Parameter.SCAN_DIRECTORY.getFlag(), "bar"});
-		assertEquals(1, testNonNullArgs.getExecutionArguments().getParsedParameters().size());
+		SuperDeDuper testScanDirLong = new SuperDeDuper(new String[] {"--directory", "scan-directory"});
+		assertNull(testScanDirLong.run());
+		assertNotNull(testScanDirLong.getExecutionArguments());
+		assertTrue(Arrays.equals(comparison, testScanDirLong.getExecutionArguments().getOptions()));
+		Option[] actualsLong = testScanDirLong.getExecutionArguments().getOptions();
+		assertEquals("scan-directory", actualsLong[0].getValue());
+		assertTrue(Arrays.equals(new String[0], testScanDirLong.getExecutionArguments().getArgs()));
+		
+		// TODO: add the actual logic for validating that the argument to the option is
+		// 1. a valid directory
+		// 2. exists
+		// 3. can be read
 	}
 
 	@Test
-	public void testRun() throws ParameterException {
+	public void testSuperDeDuper() {
+		SuperDeDuper testNullArgs = new SuperDeDuper(null);
+		assertNull(testNullArgs.run());
+		assertNotNull(testNullArgs.getExecutionArguments());
+		assertTrue(Arrays.equals(new Option[0], testNullArgs.getExecutionArguments().getOptions()));
+		assertTrue(Arrays.equals(new String[0], testNullArgs.getExecutionArguments().getArgs()));
+		// clean up
+		testNullArgs = null;
+		
+		SuperDeDuper testNonNullArgs = new SuperDeDuper(new String[] {"foo", "bar"});
+		assertNull(testNonNullArgs.run());
+		assertNotNull(testNonNullArgs.getExecutionArguments());
+		assertTrue(Arrays.equals(new Option[0], testNonNullArgs.getExecutionArguments().getOptions()));
+		String[] comparison = {"foo", "bar"};
+		assertTrue(Arrays.equals(comparison, testNonNullArgs.getExecutionArguments().getArgs()));
+	}
+
+	@Test
+	public void testRun() {
 		SuperDeDuper runner = new SuperDeDuper(null);
 		assertNull("The expected result was for a null Object to be returned", runner.run());
 	}
